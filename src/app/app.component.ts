d3a59a87d1fe0dbe2ec09f37b513bc07d11fbc3f
Volodymyr Ustilkin <volodymyr.ustilkin@gmail.com>
2021-02-24 20:50:12 +0200

    add site skeleton

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,23 @@
-import { Component } from '@angular/core';
+import {AfterViewInit, Component, ElementRef} from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements AfterViewInit {
   title = 'help-animals';
+  asideNeedShow = false;
+
+  constructor(private elRef: ElementRef) {
+  }
+
+  ngAfterViewInit(): void {
+    this.updateMode();
+  }
+
+  updateMode(): void {
+    this.asideNeedShow = this.elRef.nativeElement.firstElementChild.offsetWidth > 900;
+  }
+
 }
