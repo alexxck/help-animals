0a2e1cd6cb44cb0a333d142f4f3c515a625cde74
Anton <Mosquito>
2021-02-27 09:28:22 +0200

    header was added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,11 +5,13 @@ import { AppRouting } from './app-routing';
 import { AppComponent } from './app.component';
 import { NavMenuComponent } from './core/components/nav-menu/nav-menu.component';
 import {coreServices} from './core/services';
+import { HeaderComponent } from './header/header.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     NavMenuComponent,
+    HeaderComponent,
   ],
   imports: [
     BrowserModule,
