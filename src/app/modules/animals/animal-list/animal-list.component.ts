14aaea95bf51a8e639cd3bfa49bbaa4dba58c89e
Volodymyr Ustilkin <volodymyr.ustilkin@gmail.com>
2021-03-05 16:50:11 +0200

    add PaginationComponent to AnimalList

--- src/app/modules/animals/animal-list/animal-list.component.ts
+++ src/app/modules/animals/animal-list/animal-list.component.ts
@@ -1,31 +1,48 @@
-import {Component, OnInit} from '@angular/core';
+import {Component, OnDestroy, OnInit} from '@angular/core';
 import {environment} from '../../../../environments';
 import {HttpClient} from '@angular/common/http';
 import {IAnimalInfo} from '../models/ianimal-info';
+import {IPagination, Pagination} from '../../../shared/components/pagination/pagination.component';
+import {ActivatedRoute, Params} from '@angular/router';
+import {Subscription} from 'rxjs';
 
-const GET_ANIMALS_URL = environment.apiUrl + '/animals';
+const ANIMALS_URL = 'animals';
+const GET_ANIMALS_URL = environment.apiUrl + '/' + ANIMALS_URL;
 
 @Component({
   selector: 'app-animal-list',
   templateUrl: './animal-list.component.html',
   styleUrls: ['./animal-list.component.css']
 })
-export class AnimalListComponent implements OnInit {
+export class AnimalListComponent implements OnInit, OnDestroy {
   animalList: IAnimalInfo[] = [];
+  pagination: IPagination;
 
+  private querySubscription: Subscription;
 
-  constructor(private httpClient: HttpClient) {
-    this.getAnimals();
+  constructor(private httpClient: HttpClient, private route: ActivatedRoute) {
+    this.pagination = new Pagination();
+    this.pagination.url = '/' + ANIMALS_URL;
+
+    this.querySubscription = this.route.queryParams.subscribe(
+      (queryParam: Params) => {
+        this.pagination.page = queryParam.page;
+        this.getAnimals();
+      }
+    );
   }
 
   ngOnInit(): void {
-    // this.getAnimals();
+  }
+
+  ngOnDestroy(): void {
+    this.querySubscription.unsubscribe();
   }
 
   public getAnimals(): void {
     this.httpClient.get<IAnimalInfo[]>(GET_ANIMALS_URL).subscribe((res) => {
       this.animalList = res;
+      this.pagination.totalPages = 10; // todo set params from back
     });
   }
-
 }
--- src/app/modules/animals/animals.module.ts
+++ src/app/modules/animals/animals.module.ts
@@ -4,13 +4,14 @@ import {AnimalListComponent} from './animal-list/animal-list.component';
 import {RouterModule} from '@angular/router';
 import {AnimalsRouting} from './animals.routing';
 import {AnimalCardComponent} from './animal-card/animal-card.component';
+import {PaginationComponent} from '../../shared/components/pagination/pagination.component';
 
 
 @NgModule({
-  declarations: [AnimalListComponent, AnimalCardComponent],
+  declarations: [AnimalListComponent, AnimalCardComponent, PaginationComponent],
   imports: [
     CommonModule,
-    RouterModule.forChild(AnimalsRouting)
+    RouterModule.forChild(AnimalsRouting),
   ]
 })
 export class AnimalsModule {
