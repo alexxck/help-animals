dfc55e7622da73ff8cf657ec3795ca876d84f2dd
Volodymyr Ustilkin <volodymyr.ustilkin@gmail.com>
2021-03-12 16:49:17 +0200

    fix loader describe

--- src/app/modules/shared/components/loader/loader.component.ts
+++ src/app/modules/shared/components/loader/loader.component.ts
@@ -1,4 +1,4 @@
-import {Component, Input, OnInit} from '@angular/core';
+import {Component, Input} from '@angular/core';
 
 
 @Component({
@@ -6,15 +6,12 @@ import {Component, Input, OnInit} from '@angular/core';
   templateUrl: './loader.component.html',
   styleUrls: ['./loader.component.css']
 })
-export class LoaderComponent implements OnInit {
 
+/**
+ * fullScreen: show loader at static position center of screen
+ * show: display/hide loader
+ */
+export class LoaderComponent {
   @Input() fullScreen = false;
   @Input() show = false;
-
-  constructor() {
-  }
-
-  ngOnInit(): void {
-  }
-
 }
