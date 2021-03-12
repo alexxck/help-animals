abfc52042b64872c88b1f67196914b023ebbc9e1
Volodymyr Ustilkin <volodymyr.ustilkin@gmail.com>
2021-03-12 16:24:32 +0200

    fix loader

--- src/app/modules/shared/components/loader/loader.component.ts
+++ src/app/modules/shared/components/loader/loader.component.ts
@@ -8,7 +8,7 @@ import {Component, Input, OnInit} from '@angular/core';
 })
 export class LoaderComponent implements OnInit {
 
-  @Input() fullScreen = true;
+  @Input() fullScreen = false;
   @Input() show = false;
 
   constructor() {
