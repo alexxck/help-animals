20df98e40b4c7fa321d7d2df8f8dc3c7d8a9d36d
Volodymyr Ustilkin <volodymyr.ustilkin@gmail.com>
2021-03-13 11:44:44 +0200

    fix add field 'name'

--- src/app/modules/animals/models/ianimal-info.ts
+++ src/app/modules/animals/models/ianimal-info.ts
@@ -1,11 +1,10 @@
 export interface IAnimalInfo {
   id: string;
+  name: string;
   breed: string;
   age: number;
   state: string;
   color: string;
   features: string;
   imgUrl: string;
-  dateAdded: string;
-  dateLastEdit: string;
 }
